ae8cb75575be16aeeddcf8e28e4b4ed9f9db5c0b
Angel Luis Morales Hernandez <alu0100888157@ull.edu.es>
2017-02-19 22:42:02 +0000

    corrigiendo otro fallo 3 de fichero.js

--- fichero.js
+++ fichero.js
@@ -1,11 +1,9 @@
 //importar
 var express = require('express');
-
-var app = express()
-var path = require('path')
-//app.set('port', (process.env.PORT || 8080));
+var app = express();
+var path = require('path');
+app.set('port',(process.env.PORT || 8080));
 //instanciar
-//var app = express();
 
 //ruteo
 app.get('/', function(req, res){
@@ -14,9 +12,37 @@ app.get('/', function(req, res){
 app.use(express.static(path.join(__dirname,'gh-pages')));
 
 app.get('/', function(req, res){
-  res.send('hello world');
+  res.send('index');
 });
 //escuchar
-app.listen(9000);
+app.listen(app.get('port'),function(){
+
+console.log("Servidor Express escuchando:", app.get('port'));
+});
 
 //console.log("Servidor Express escuchando en modo %s", app.setings.env);
+
+
+
+// //importar
+// var express = require('express');
+
+// var app = express()
+// var path = require('path')
+// //app.set('port', (process.env.PORT || 8080));
+// //instanciar
+// //var app = express();
+
+// //ruteo
+// app.get('/', function(req, res){
+//     res.sendfile(__dirname + '/gh-pages/index.html');
+// });
+// app.use(express.static(path.join(__dirname,'gh-pages')));
+
+// app.get('/', function(req, res){
+//   res.send('hello world');
+// });
+// //escuchar
+// app.listen(9000);
+
+// //console.log("Servidor Express escuchando en modo %s", app.setings.env);

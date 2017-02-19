02a9877171dfa4e78ac7594a0c3655f83e11d531
Angel Luis Morales Hernandez <alu0100888157@ull.edu.es>
2017-02-19 22:03:25 +0000

    corrigiendo fichero.js

--- fichero.js
+++ fichero.js
@@ -1,6 +1,7 @@
 //importar
 var express = require('express');
-
+var app = express();
+var path = require('path');
 //instanciar
 var app = express();
 
@@ -8,8 +9,12 @@ var app = express();
 app.get('/', function(req, res){
     res.sendfile(__dirname + '/gh-pages/index.html');
 });
+app.use(express.static(path.join(__dirname,'gh-pages')));
 
+app.get('/', function(req, res){
+  res.send('hello world');
+});
 //escuchar
 app.listen(9000);
 
-console.log("Servidor Express escuchando en modo %s", app.setings.env);
+//console.log("Servidor Express escuchando en modo %s", app.setings.env);
